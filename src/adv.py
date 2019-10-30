77b316070634967ae14d9af07cbd8ba55bbbc378
onetrckchris <onetrckchris@gmail.com>
2019-10-30 16:04:02 -0700

    sort of done

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,19 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons."),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
-
 
 # Link rooms together
 
@@ -49,3 +42,57 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def die():
+    return "\n--- Your vision goes hazy and in an instant you fall to the ground... Lifeless.\nPlease only select from valid inputs.\n"
+
+player_name = input("Please enter your name: ")
+
+player1 = Player(player_name, room['outside']) # Creating the player. They start outside always.
+
+print(f"\n\nWelcome to my adventure game, {player1.name}!")
+print(f"--- You're currently at the {player1.current_room.name.lower()}.")
+print(f"----- {player1.current_room.description}")
+
+# Doing it the super inefficient way first. Just to get this done.
+user_input = input("[w] Walk forth to the foyer...\n[q] -- Quit game --\n: ")
+while user_input != 'q':
+    # (Outside) Player walks north to the foyer.
+    if user_input == 'w':
+        player1.current_room = room['foyer']
+        print(f"\n--- You're currently at the {player1.current_room.name.lower()}.")
+        print(f"----- {player1.current_room.description}")
+        user_input = input("[w] Walk up the stairs through the north passage.\n[d] Make your way through the seemingly narrow east passage.\n[s] Head back to the cave entrance.\n[q] -- Quit game --\n: ")
+        # (Foyer) Player walks north to the overlook.
+        if user_input == 'w':
+            player1.current_room = room['overlook']
+            print(f"\n--- You're currently at the {player1.current_room.name.lower()}.")
+            print(f"----- {player1.current_room.description}")
+            user_input = input("[s] Walk back to the foyer.\n[q] -- Quit game --\n: ")
+            # (Overlook) Player walks south to foyer.
+            if user_input == 's':
+                player1.current_room = room['foyer']
+                print(f"\n--- You're currently at the {player1.current_room.name.lower()}.")
+                print(f"----- {player1.current_room.description}")
+                user_input = input("[w] Walk up the stairs through the north passage.\n[d] Make your way through the seemingly narrow east passage.\n[s] Head back to the cave entrance.\n[q] -- Quit game --\n: ")
+        # (Foyer) Player walks east through the narrow passage.
+        elif user_input == 'd':
+            player1.current_room = room['narrow']
+            print(f"\n--- You're currently walking through a {player1.current_room.name.lower()}.")
+            print(f"----- {player1.current_room.description}")
+            user_input = input("[w] Follow the smell of gold.\n[a] Walk back to the foyer.\n[q] -- Quit game --\n: ")
+            if user_input == 'w':
+                player1.current_room = room['treasure']
+                print(f"\n--- You're currently in the {player1.current_room.name.lower()}.")
+                print(f"----- {player1.current_room.description}")
+                print("\nSorry about no treasure! :( But you beat the game, good job!")
+                break
+        # (Foyer) Player walks south back outside.
+        elif user_input == 's':
+            player1.current_room = room['outside']
+            print(f"\n--- You're currently at the {player1.current_room.name.lower()}.")
+            print(f"----- {player1.current_room.description}")
+            user_input = input("[w] Walk forth to the foyer...\n[q] -- Quit game --\n: ")
+    else:
+        print(die())
+        break
--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+    def __repr__(self):
+        return f"{repr(self.name)}"
+    
+    def check_direction(self, direction):
+        if self.__getattribute__(direction) != None:
+            return self.__getattribute__(direction)
+        else:
+            return 'N/A'
